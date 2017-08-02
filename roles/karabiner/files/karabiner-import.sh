78b2d17dc1384147e5d7d591dfe0262c03c19943
Daniel Banck <daniel@dbanck.de>
2017-08-02 15:10:16 +0200

    replace seil and karabiner with karabiner-elements

--- roles/karabiner/files/karabiner-import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
-
-$cli set remap.uk_saneuklayout2 1
-/bin/echo -n .
-$cli set repeat.initial_wait 250
-/bin/echo -n .
-/bin/echo
--- roles/seil/files/seil-import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cli=/Applications/Seil.app/Contents/Library/bin/seil
-
-$cli set enable_capslock 1
-/bin/echo -n .
-$cli set keycode_capslock 59
-/bin/echo -n .
