6ea7fb4c8f76b0004b750961f50a1e7c878c61a2
Daniel Banck <daniel@dbanck.de>
2017-11-01 09:55:56 +0100

    update fish config with correct paths

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -10,13 +10,12 @@ set -x LANG en_US.UTF-8
 set -x GOPATH ~/Projects/go
 
 set -x PATH /usr/local/sbin ~/.rbenv/shims $PATH ~/Applications/bin ~/.npm/bin
-set -x PATH $PATH $GOPATH/bin ~/Projects/weluse/snippets/bin ~/.gem/ruby/2.0.0/bin
+set -x PATH $PATH $GOPATH/bin
 
 set -x EDITOR /usr/local/bin/vim
 
-set -x ANDROID_HOME /usr/local/opt/android-sdk
-set -x ANDROID_SDK /usr/local/opt/android-sdk
-set -x ANDROID_NDK /usr/local/opt/android-ndk
+set -x ANDROID_HOME /usr/local/share/android-sdk
+set -x ANDROID_SDK_ROOT /usr/local/share/android-sdk
 
 # Color fixes for nova colorscheme
 set fish_color_autosuggestion '#899BA6'
