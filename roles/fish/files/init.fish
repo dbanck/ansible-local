1e3e252d16090ceb4baff850a7124dd50c5f0f3a
Daniel Banck <daniel@dbanck.de>
2020-04-27 23:53:25 +0200

    update fish installation

---
+++ roles/fish/files/init.fish
@@ -0,0 +1,9 @@
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+set -x GOPATH ~/Projects/go
+
+set -x PATH /usr/local/sbin ~/.rbenv/shims $PATH ~/Applications/bin ~/.npm/bin
+set -x PATH $PATH $GOPATH/bin
+
+set -x EDITOR /usr/local/bin/vim
