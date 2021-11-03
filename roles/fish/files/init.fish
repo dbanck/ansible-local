0a682fd2994111c841f954d6281376cb28ba9cfc
Daniel Banck <daniel@dbanck.de>
2021-11-03 14:50:06 +0100

    enable roles and update go path

--- roles/fish/files/init.fish
+++ roles/fish/files/init.fish
@@ -1,7 +1,7 @@
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-set -x GOPATH ~/Projects/go
+set -x GOPATH ~/go
 
 set -x PATH /usr/local/sbin ~/.rbenv/shims $PATH ~/Applications/bin ~/.npm/bin
 set -x PATH $PATH $GOPATH/bin
